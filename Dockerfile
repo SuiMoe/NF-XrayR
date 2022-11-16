4c87e552518e99ae09f1b55db393106ad972dbf7
AkashiCoin <99643950+SuiMoe@users.noreply.github.com>
2022-11-17 02:41:58 +0800

    merge: XrayR

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM alpine:edge
 
-ADD start.sh /start.sh
+ADD . /
 RUN apk update && \
-    apk add --no-cache ca-certificates unzip wget && \
+    apk add --no-cache ca-certificates caddy unzip wget curl && \
     wget -q https://github.com/XrayR-project/XrayR/releases/download/v0.8.4/XrayR-linux-64.zip  && \
-	echo "y" | unzip XrayR-linux-64.zip && \
+	unzip -o XrayR-linux-64.zip && \
     chmod +x /start.sh /XrayR
 
 EXPOSE 443
--- start.sh
+++ start.sh
@@ -56,4 +56,30 @@ Nodes:
           ALICLOUD_SECRET_KEY: bbb
 EOF
 
-/XrayR -config config.yml
+if [ -f $CADDYIndexPage ];then
+  CADDYIndexPage="https://github.com/AYJCSGM/mikutap/archive/master.zip"
+fi
+
+# configs
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+cat "/Caddyfile" | sed -e "1c :$PORT" >/etc/caddy/Caddyfile
+
+
+/XrayR -config config.yml &
+
+sleep 5
+
+if [ -n $(pgrep XrayR) ];then
+  echo "\033[32m[INFO]\033[0m XrayR is running"
+else
+  echo "\033[31m[ERROR]\033[0m XrayR is not running"
+  exit 1
+fi
+
+port=$(netstat -nltp | grep XrayR | awk '{print $4}' | sed 's/://g')
+
+sed -i "s/8080/${port}/g" /etc/caddy/Caddyfile
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
